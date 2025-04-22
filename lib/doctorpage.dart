9b7c4b3cbb608353fda6773457a1109d6c873ed9
unknown <karkiaku000@gmail.com>
2025-04-22 14:37:03 +0545

    doctor page

---
+++ lib/doctorpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Doctorpage extends StatefulWidget {
+  const Doctorpage({super.key});
+
+  @override
+  State<Doctorpage> createState() => _DoctorpageState();
+}
+
+class _DoctorpageState extends State<Doctorpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
