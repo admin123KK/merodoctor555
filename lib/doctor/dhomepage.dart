bb2e2be68fa38b5ba02c522510afc7118a28e5d5
unknown <karkiaku000@gmail.com>
2025-04-30 09:33:15 +0545

    doctor homepage

--- lib/doctor/dhomepage.dart
+++ lib/doctor/dhomepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:merodoctor/doctor/doctordetailspage.dart';
 import 'package:merodoctor/doctor/dprofilepage.dart';
 
 class Dhomepage extends StatefulWidget {
@@ -33,7 +32,7 @@ class _DhomepageState extends State<Dhomepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 65),
@@ -43,31 +42,30 @@ class _DhomepageState extends State<Dhomepage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
-                    'Find your desire \nhealth Solution',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    'Hello Doctor',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(height: 6),
                   Text(
-                    '$greeting,Dr.Sky',
-                    style: const TextStyle(color: Colors.grey),
+                    '$greeting, Dr.Sky',
+                    style: const TextStyle(color: Colors.black),
                   ),
                   const SizedBox(height: 20),
                   Container(
                     height: 40,
                     decoration: const BoxDecoration(
-                      color: Colors.grey,
+                      color: const Color.fromARGB(93, 28, 165, 172),
                       borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(20),
                         topRight: Radius.circular(22),
                       ),
                     ),
                     child: const TextField(
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.white,
+                      style: TextStyle(color: Colors.black),
+                      cursorColor: Colors.black,
                       decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search, color: Colors.white),
+                        prefixIcon: Icon(Icons.search, color: Colors.black),
                         hintText: 'Search...',
-                        hintStyle: TextStyle(color: Colors.white70),
+                        hintStyle: TextStyle(color: Colors.grey),
                         border: InputBorder.none,
                         contentPadding: EdgeInsets.symmetric(vertical: 10),
                       ),
@@ -76,204 +74,44 @@ class _DhomepageState extends State<Dhomepage> {
                   const SizedBox(
                     height: 30,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: const Icon(
-                              Icons.person_search_outlined,
-                              size: 30,
-                            ),
-                          ),
-                          const Text(
-                            'Doctor',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: const Icon(
-                              Icons.medication_outlined,
-                              size: 30,
-                            ),
-                          ),
-                          const Text(
-                            'Pharmacy',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: const Icon(
-                              Icons.warning_amber_outlined,
-                              size: 30,
-                            ),
-                          ),
-                          const Text(
-                            'Emergency',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: const Icon(
-                              Icons.local_hospital_outlined,
-                              size: 30,
-                            ),
-                          ),
-                          const Text(
-                            'Hospital',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
                   Container(
-                    height: 230,
+                    height: 670,
                     width: double.infinity,
                     decoration: BoxDecoration(
-                      color: const Color.fromARGB(90, 28, 165, 172),
                       borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(
                       children: [
-                        Column(
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 10),
-                              child: Text(
-                                'Early Protection for \n your family',
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: const Text(
+                                'Welcome, \nDr.Abiskar Gyawali',
                                 style: TextStyle(
                                     fontWeight: FontWeight.bold, fontSize: 22),
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.all(13.0),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 10),
                               child: Container(
-                                height: 25,
-                                width: 100,
+                                height: 70,
+                                width: 70,
                                 decoration: BoxDecoration(
-                                    color: const Color(0xFF1CA4AC),
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: const Center(
-                                  child: Text(
-                                    'Learn more',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
+                                  color: Color.fromARGB(93, 28, 165, 172),
+                                  borderRadius: BorderRadius.circular(60),
+                                ),
+                                child: Image.asset(
+                                  'assets/image/startpage3.png',
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                            ),
+                            )
                           ],
                         ),
-                        Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 7, vertical: 0),
-                            child: Container(
-                              height: 130,
-                              width: 130,
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(70)),
-                              child: Image.asset(
-                                'assets/image/startpage3.png',
-                                fit: BoxFit.cover,
-                              ),
-                            )),
-                      ],
-                    ),
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'See all',
-                          style: TextStyle(color: Color(0xFF1CA4AC)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Text(
-                    'Top Doctor',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        _buildDoctorCard(
-                          ('assets/image/startpage1.png'),
-                          'Dr.SKy Karki',
-                          'Cardiologist',
-                          () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const Doctordetailspage()));
-                          },
-                        ),
-                        _buildDoctorCard(
-                          ('assets/image/startpage3.png'),
-                          'Dr.Abiskar Gyawali',
-                          'Orthopedist',
-                          () {},
-                        ),
-                        _buildDoctorCard(
-                          ('assets/image/startpage3.png'),
-                          'Ritesh Ac',
-                          'Psychologist',
-                          () {},
-                        )
                       ],
                     ),
                   ),
