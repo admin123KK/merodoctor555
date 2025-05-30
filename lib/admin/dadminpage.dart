cf83c744a31fab6e9499204b19a09605b7b1632e
unknown <karkiaku000@gmail.com>
2025-05-30 18:34:26 +0545

    Manage Doctors

--- lib/admin/dadminpage.dart
+++ lib/admin/dadminpage.dart
@@ -11,6 +11,7 @@ class _DAdminPageState extends State<DAdminPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.white,
       body: Column(
         children: [
           const SizedBox(
@@ -31,14 +32,34 @@ class _DAdminPageState extends State<DAdminPage> {
                       size: 27,
                     )),
                 const Text(
-                  'Doctor Manage',
+                  'Manage Doctors',
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                 ),
                 const Icon(
                   Icons.more_vert_outlined,
                   size: 30,
-                )
+                ),
               ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Container(
+              height: 40,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(12)),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    hintText: 'Search...',
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: Colors.black54)),
+              ),
             ),
           )
         ],
